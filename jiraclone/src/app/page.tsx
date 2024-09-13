74b18fe02a06c98ae527801e2aa405790d59bb7a
Aryanjangde <aryan.jangde821@gmail.com>
2024-09-13 15:48:08 +0530

    ..

--- jiraclone/src/app/page.tsx
+++ jiraclone/src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import CreateIssue from "../components/CreateIssue"
+// import Image from "next/image";
+// import CreateIssue from "../components/CreateIssue"
 import Sidebar from "../components/Sidebar"
 
 export default function Home() {
