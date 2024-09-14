d01852aac42573d6f4bc04f11ce1d6b580d3eecb
Karthikreddy0165 <Karthikredddy0165@gmail.com>
2024-09-14 23:04:01 +0530

    resolved typeErrors

--- jiraclone/src/app/api/users/route.js
+++ jiraclone/src/app/api/users/route.js
@@ -1,24 +1,23 @@
 import prisma from '../../../../lib/prisma'
-export default async function handler(req, res) {
-    if (req.method === 'GET') {
-      try {
-        const users = await prisma.user.findMany()
-        res.status(200).json(users)
-      } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch users' })
-      }
-    } else if (req.method === 'POST') {
-      try {
-        const { name, email, role } = req.body
-        const user = await prisma.user.create({
-          data: { name, email, role },
-        })
-        res.status(201).json(user)
-      } catch (error) {
-        res.status(400).json({ error: 'Failed to create user' })
-      }
-    } else {
-      res.setHeader('Allow', ['GET', 'POST'])
-      res.status(405).end(`Method ${req.method} Not Allowed`)
-    }
+import { NextResponse } from 'next/server'
+
+export async function GET(){
+  try {
+    const users = await prisma.user.findMany()
+    NextResponse.json({data : users}, {status: 200})
+  } catch (error) {
+    NextResponse.json({ error: 'Failed to fetch users' }, {status: 500})
+  }
+}
+
+export async function POST(){
+  try {
+    const { name, email, role } = await req.json()
+    const user = await prisma.user.create({
+      data: { name, email, role },
+    })
+    NextResponse.json({data : user}, {status: 201})
+  } catch (error) {
+    NextResponse.json({ error: 'Failed to create user' }, {status: 400})
+  }
 }
