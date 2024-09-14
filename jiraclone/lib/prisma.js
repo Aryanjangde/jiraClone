4cf3a9acce2423fba62d783a94d50bc4bc80c4ad
Karthikreddy0165 <Karthikredddy0165@gmail.com>
2024-09-14 15:53:47 +0530

    Update Prisma schema, modify API routes, and update dependencies

--- jiraclone/lib/prisma.js
+++ jiraclone/lib/prisma.js
@@ -1,14 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-let prisma
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
-}
-
-export default prisma
+const prisma = new PrismaClient();
+export default prisma 
--- jiraclone/src/app/api/projects/route.js
+++ jiraclone/src/app/api/projects/route.js
@@ -1,7 +1,5 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import prisma from '../../../../lib/prisma'
 import { NextResponse } from 'next/server'
-
 
 export async function GET() {
     try {
